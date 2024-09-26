e0b855992d8e1fc987aeb1deae246c2d487ab7c2
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:51:47 +0300

    Add project files.

---
+++ Difference of squares/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Difference_of_squares
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Введіть число N:");
+            int N = int.Parse(Console.ReadLine());
+
+            
+            int sum = Enumerable.Range(1, N).Sum(); 
+            int squareOfSum = sum * sum; 
+
+            
+            int sumOfSquares = Enumerable.Range(1, N).Select(x => x * x).Sum();
+
+           
+            int difference = squareOfSum - sumOfSquares;
+
+            
+            Console.WriteLine($"Різниця між квадратом суми і сумою квадратів перших {N} натуральних чисел: {difference}");
+        }
+    }
+}
